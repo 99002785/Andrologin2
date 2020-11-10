2bede50e78ae52e61446b3118c4425401c8a6baa
99002785 <visvakumar.s@ltts.com>
2020-11-10 10:03:49 +0530

    Second

--- app/src/main/java/com/example/activity/HomeActivity.java
+++ app/src/main/java/com/example/activity/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent  = getIntent();
+        Bundle extras = intent.getExtras();
+        String data  = extras.getString("myKey");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(data);
     }
 }
