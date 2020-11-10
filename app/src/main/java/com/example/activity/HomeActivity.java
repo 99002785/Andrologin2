b952aa8ad1d5499a4fe4f1e2b4db1cf6227a953a
99002785 <visvakumar.s@ltts.com>
2020-11-10 12:20:37 +0530

    Listview

--- app/src/main/java/com/example/activity/HomeActivity.java
+++ app/src/main/java/com/example/activity/HomeActivity.java
@@ -4,18 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
+
+    String[] languages;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        languages = new String[]{"english","hindi","urdu","kannada"};
         Intent intent  = getIntent();
         Bundle extras = intent.getExtras();
         String data  = extras.getString("myKey");
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textViewResult);
         textView.setText(data);
+
+        ListView countriesListView = findViewById(R.id.countriesListview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, //layout file of each row in the listview
+                languages);
+        countriesListView.setAdapter(adapter);
     }
+
+
+
 }
