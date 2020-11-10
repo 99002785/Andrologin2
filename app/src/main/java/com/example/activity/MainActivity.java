4dbb948654401affe1b993977572dc9bdcb61afd
99002785 <visvakumar.s@ltts.com>
2020-11-10 11:52:07 +0530

    Third

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -3,20 +3,57 @@ package com.example.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onpause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onresume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onstop");
+
     }
 
     public void clickHandler(View view) {
+        Log.e(TAG,"clickHandler");
         Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
         hIntent.putExtra("myKey","Visva");
+        int c = add(10,20);
         startActivity(hIntent);
+    }
+    private  int add(int a, int b){
+        return a+b;
     }
 }
