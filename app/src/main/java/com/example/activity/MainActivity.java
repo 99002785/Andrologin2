b3459808e329acd5a5aed663f0962d58b1393026
99002785 <visvakumar.s@ltts.com>
2020-11-10 12:20:37 +0530

    Fourth

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
         Log.i(TAG,"onStart");
     }
 
@@ -48,11 +50,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickHandler(View view) {
         Log.e(TAG,"clickHandler");
+        switch(view.getId()){
+            case R.id.login:
+                startHome();
+                break;
+            case R.id.cancel:
+                dailermeth();
+                break;
+        }
+
+
+    }
+
+    private void dailermeth() {
+        Intent intentNew = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:12345678"));
+        startActivity(intentNew);
+    }
+
+    private void startHome() {
         Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
         hIntent.putExtra("myKey","Visva");
         int c = add(10,20);
         startActivity(hIntent);
     }
+
     private  int add(int a, int b){
         return a+b;
     }
